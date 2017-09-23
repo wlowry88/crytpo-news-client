65bacc91a08430a7abbd3460fb045087dbcb35cd
Will Lowry <william.c.lowry@gmail.com>
2017-09-23 17:29:21 -0400

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -62,7 +62,6 @@ class App extends Component {
 
   render() {
     const { searchTerm, result } = this.state;
-    if (!result) { return null; }
     return (
       <div className="page">
         <div className="interactions">
@@ -72,11 +71,13 @@ class App extends Component {
           >Search
           </Search>
         </div>
-        <Table 
-          list={result.hits}
-          pattern={searchTerm}
-          onDismiss={this.onDismiss}
-        />
+        { result &&
+          <Table 
+            list={result.hits}
+            pattern={searchTerm}
+            onDismiss={this.onDismiss}
+          />
+        }
       </div>
     );
   }
