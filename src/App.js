479dc1200af2c7f0c36bdb5c80d9d48e407fbf77
Will Lowry <william.c.lowry@gmail.com>
2017-09-23 14:13:32 -0400

    using some arrow functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import React, { Component } from 'react';
-import WelcomeMessage from './WelcomeMessage.js';
 import logo from './logo.svg';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+}, {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+}, ];
+
+
 class App extends Component {
   render() {
-    const randomNumber = parseInt(10000 * Math.random());
+    const randomNumber = parseInt(10000 * Math.random(), 10);
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <WelcomeMessage firstName = "Marcuss" lastName = 'Aurelius' />
+          { list.map(item =>
+              <div key={item.objectID}>
+                <span>
+                  <a href={item.url}>{item.title}</a>
+                </span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+              </div>
+            )
+          }
         </div>
-        <p className="App-intro">
-          This site will send texts to phone numbers to start.
-        </p>        
-        <p className="App-intro">
-          {randomNumber} requests sent!
-        </p>
       </div>
     );
   }
